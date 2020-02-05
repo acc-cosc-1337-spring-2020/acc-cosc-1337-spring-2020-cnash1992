cd0464cde816b731a46ce32c29fc53fe4a62a7d4
cnash1992 <chris.nash2@g.austincc.edu>
2020-02-04 18:39:50 -0600

    Bool expressions cin and if examples

--- src/examples/01_module/01_output/main.cpp
+++ src/examples/01_module/01_output/main.cpp
@@ -5,6 +5,27 @@ using std::cout;
 
 int main() 
 {
+	int n1 = 5, n2 = 10;
+	if (6 >= n1 &&  6 <= n2)
+	{
+		//6 is in range
+	}
+
+	if (6 >= n1 || 6 <= n2)
+	{
+		//6 outside range
+	}
+
+	if (!(6 >= n1 && 6 <= n2))
+	{
+		//6 not in range
+	}
+
+	if (6 != n1)
+	{
+		//6 is not in range
+	}
+
 	cout<<"Hello World!";
 
 	return 0;
