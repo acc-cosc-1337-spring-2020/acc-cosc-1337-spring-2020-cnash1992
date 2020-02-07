de40b262b59cfab0372cea50fd83bbdff855e776
cnash1992 <chris.nash2@g.austincc.edu>
2020-02-06 18:22:39 -0600

    Switch boolean and menu function

--- test/examples_test/02_module_test/02_module_tests.cpp
+++ test/examples_test/02_module_test/02_module_tests.cpp
@@ -2,6 +2,7 @@
 #include "catch.hpp"
 #include "if_else.h"
 #include "if.h"
+#include "switch.h"
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
@@ -23,3 +24,12 @@ TEST_CASE("Test get generation function")
 	REQUIRE(get_generation(1000) == "Invalid Year");
 }
 
+TEST_CASE("Test menu function") 
+{
+	REQUIRE(menu(0) == "Invalid Option");
+	REQUIRE(menu(1) == "Option 1");
+	REQUIRE(menu(2) == "Option 2");
+	REQUIRE(menu(3) == "Option 3");
+	REQUIRE(menu(4) == "Option 4");
+	REQUIRE(menu(5) == "Invalid Option");
+}
