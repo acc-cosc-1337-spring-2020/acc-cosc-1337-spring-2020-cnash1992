ba6cadc34172e9a3e9317f064b084492a8b432d8
cnash1992 <chris.nash2@g.austincc.edu>
2020-02-11 18:23:45 -0600

    While loop examples for sum of squares

--- src/examples/03_module/01_while/main.cpp
+++ src/examples/03_module/01_while/main.cpp
@@ -1,7 +1,17 @@
 #include "while.h"
+#include <iostream>
+
+using std::cout; using std::cin;
 
 int main() 
 {
+	int num, sum;
+	
+	cout << "Enter a number: ";
+	cin >> num;
+
+	sum = sum_of_squares(num);
+	cout << "Sum if squares for " << num << " is " << sum;
 
 	return 0;
 }
--- src/examples/03_module/01_while/while.cpp
+++ src/examples/03_module/01_while/while.cpp
@@ -1,8 +1,23 @@
 #include "while.h"
-//Using a while loop write code for sum_of_squares function that accepts an int and returns  
+#include "while.h"
+/*Using a while loop write code for sum_of_squares function that accepts an int and returns  
 //the sum of all the squares of that number from 1 to the number. 
 //Example: sum_of_squares(4) 
-//1*1 + 2 * 2 + 3*3 + 4*4 = 30
+//1*1 + 2 * 2 + 3*3 + 4*4 = 30*/
+int sum_of_squares(int num)
+{
+	int count = 1;
+	int sum = 0;
+	
+	while (count <= num) 
+	{
+		sum = sum + (count * count);
+		count = count + 1;
+	}
+ 
+	return sum;
+}
+
 
 
 //Using a while loop write code for display function that accepts a string and displays 
@@ -18,4 +33,3 @@ l
 0
 
 */
-
