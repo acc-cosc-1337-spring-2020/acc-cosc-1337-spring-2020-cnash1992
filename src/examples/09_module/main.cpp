0f0c05cf2e73a162dcc0327c20e002e613a9ad2e
chris nash <nashjeffreyr@centurytel.net>
2020-04-14 18:50:56 -0500

    File reading and writing with temperature and temperature data classes.

--- src/examples/09_module/main.cpp
+++ src/examples/09_module/main.cpp
@@ -1,6 +1,20 @@
+#include "temperature.h"
+#include "temperature_data.h"
 
 int main() 
 {
+	vector <Temperature> out_temps{ Temperature(1, 98.9), Temperature(2, 99.5),
+		Temperature(3, 99) };
+
+	TemperatureData data;
+
+	data.save_temps(out_temps);
+	vector<Temperature> in_temps = data.get_temps();
+
+	for (auto temp : in_temps) 
+	{
+		std::cout << temp.get_hour() << " " << temp.get_reading() << "\n";
+	}
 
 	return 0;
 }
--- src/examples/09_module/temperature_data.h
+++ src/examples/09_module/temperature_data.h
@@ -1,1 +1,24 @@
 //temperature_data.h
+#include "temperature.h"
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+using std::string; using std::vector; using std::cin; using std::cout; 
+using std::fstream;
+
+#ifndef TEMPERATURE_DATA_H
+#define TEMPERATURE_DATA_H
+
+class TemperatureData 
+{
+public:
+	void save_temps(vector<Temperature>& ts);
+	vector<Temperature> get_temps()const;
+
+private:
+	const string file_name{ "temperature.dat" };
+};
+
+#endif // TEMPERATURE_DATA_H
