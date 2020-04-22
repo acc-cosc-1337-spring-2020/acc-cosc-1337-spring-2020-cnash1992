b614faf0a37b5257c91fd1b43c4582efa3203853
chris nash <nashjeffreyr@centurytel.net>
2020-04-21 19:11:17 -0500

    started creating Vector class.

--- src/classwork/11_assign/vector.h
+++ src/classwork/11_assign/vector.h
@@ -1,2 +1,20 @@
 //
-//TODO 7 add template
+
+#ifndef MY_VECTOR_H
+#define MY_VECTOR_H
+#include <vector>
+
+class Vector 
+{
+public:
+	Vector(size_t sz);
+	Vector(const Vector& v); // copy constructor
+	size_t Size()const { return size; }
+	int& operator[](int i) { return nums[i]; }
+	int& operator[](int i)const { return nums[i]; }
+private:
+	size_t size;
+	int* nums;
+};
+
+#endif // MY_VECTOR_H
